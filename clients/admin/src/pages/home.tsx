8e5430262efcb6c72ff108497d6efc74806e27a6
m3rashid <m3rashid.hussain@gmail.com>
2024-01-18 17:08:41 +0530

    finally component library done

--- clients/admin/src/pages/home.tsx
+++ clients/admin/src/pages/home.tsx
@@ -1,10 +1,15 @@
 import { Button } from '@admin/components/ui/button';
+import ModeToggle from '@admin/header/modeToggle';
+import { ColorModeProvider } from '@kobalte/core';
 
 const HomePage = () => {
   return (
     <div>
-      <div>hello</div>
-      <Button variant='destructive'>Click me</Button>
+      <ColorModeProvider>
+        <div>hello</div>
+        <ModeToggle />
+        <Button variant='destructive'>Click me</Button>
+      </ColorModeProvider>
     </div>
   );
 };
