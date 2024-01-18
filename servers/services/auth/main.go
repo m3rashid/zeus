f0610ea5c1bbbb6a8e2e3dfea6b2455925f9af7b
m3rashid <m3rashid.hussain@gmail.com>
2024-01-18 09:10:54 +0530

    backend update

---
+++ servers/services/auth/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"auth/config"
+	"auth/discovery"
+	"log"
+	"os"
+	"utils"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.Server = utils.Server{
+		Name:       "auth",
+		ServerIP:   "localhost",
+		Modules:    []utils.Module{},
+		ServerPort: os.Getenv("PORT"),
+		CorsConfig: cors.Config{},
+	}
+
+	go discovery.DiscoverServers()
+
+	config.Server.SetupStructuredLogger()
+
+	app := fiber.New(config.Server.GetDefaultFiberConfig(fiber.Config{}))
+	app.Use(cors.New(config.Server.CorsConfig))
+
+	app.Get(utils.DISCOVERY_ENDPOINT, config.Server.DiscoveryHandler)
+	app.Get("/api/discover", discovery.DiscoverResourceServers)
+
+	log.Printf("Starting %s server ...\n", config.Server.Name)
+	app.Listen(":" + config.Server.ServerPort)
+}
